dab43c7685fb972f12a9d1bf1994bd9a8c27bc4a
akshay-kumar-being-infinity <akshay@being-infinity.com>
2026-07-17 12:46:14 +0530

    initialised springboot project

---
+++ src/main/java/com/being_infinity/AutoSuggest/AutoSuggestApplication.java
@@ -0,0 +1,13 @@
+package com.being_infinity.AutoSuggest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSuggestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoSuggestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/being_infinity/AutoSuggest/AutoSuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.being_infinity.AutoSuggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
