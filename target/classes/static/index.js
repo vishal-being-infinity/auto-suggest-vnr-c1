4f9e7213ca28b1cc7af4a5fe63105094538a5aa8
akshay-kumar-being-infinity <akshay@being-infinity.com>
2026-07-18 12:07:45 +0530

    implemented toggle users feature

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -1,3 +1,36 @@
 // alert("Hello guysss");
 
 console.log("hello from JS!")
+
+var users = [
+    {
+        "name" : "John Doe",
+        "gender" : "Male",
+        "img" : "john.png"
+    },
+    {
+        "name" : "Jane Doe",
+        "gender" : "Female",
+        "img" : "jane.png"
+    }
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
+    // get the elements
+
+    // 1: image
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    // 2: name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    // 3: gender
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+
+}
