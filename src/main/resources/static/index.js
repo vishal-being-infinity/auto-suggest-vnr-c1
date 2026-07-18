56551f05b815d6b6f71e28e420cb9f8a863ffc7a
akshay-kumar-being-infinity <akshay@being-infinity.com>
2026-07-18 13:30:20 +0530

    random

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -34,3 +34,33 @@ function toggleUser(){
     userGender.innerHTML = users[id].gender;
 
 }
+
+function randomUser(){
+    fetch('https://randomuser.me/api/')
+        .then(function(response){
+            // convert raw text to json
+            return response.json();
+        })
+        // dom mannipulation with API data
+        .then(function(data){
+
+            var userData = data.results[0];
+
+            // 1: image
+            var userImage = document.getElementById("user-image");
+            userImage.src = userData.picture.large;
+
+            // 2: name
+            var userName = document.getElementById("user-name");
+            userName.innerHTML = userData.name.first + " " +
+                                    userData.name.last;
+
+            // 3: gender
+            var userGender = document.getElementById("user-gender");
+            userGender.innerHTML = userData.gender;
+        })
+        // handle error
+        .catch(function(err){
+            console.log(err);
+        })
+}
